0033565d871579abc207ba599eeaaa82b95e459f
c083072 <prmarches@gmail.com>
2021-10-20 17:55:18 -0300

    iniciando JPA com hibernate

---
+++ loja/src/main/java/br/com/alura/modelo/Produto.java
@@ -0,0 +1,55 @@
+package br.com.alura.modelo;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+     public Long getId(){
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public BigDecimal getPreco() {
+        return preco;
+    }
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+
+
+
+}
